6e7d2fec16b68405610b6082768c01d66e74e565
RUDRA K SHARMA <RUDRANU1318@GMAIL.COM>
2025-02-14 13:19:21 +0530

    Add files via upload

---
+++ a1.cpp
@@ -0,0 +1,27 @@
+//Create a program that reads a user's name and age, then outputs: "Hello [name], you are [age] years old!"
+
+#include<iostream>
+#include <string>
+using namespace std;
+
+
+int main() {
+    string name;
+    int age;
+    cout << "Enter your name : ";
+    getline(cin, name);
+    cout << "Enter your age : "; 
+    cin >> age;
+    cout << "Hello " << name << ", you are " << age << " years old." << endl;
+
+
+    return 0;
+}
+
+
+
+
+
+
+
+

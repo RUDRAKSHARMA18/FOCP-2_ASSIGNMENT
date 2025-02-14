873b4414643fb9b4981dc559dcb0a45f6e298aa3
RUDRA K SHARMA <RUDRANU1318@GMAIL.COM>
2025-02-14 13:26:11 +0530

    Update a1.cpp

--- a1.cpp
+++ a1.cpp
@@ -1,23 +1,3 @@
-//Create a program that reads a user's name and age, then outputs: "Hello [name], you are [age] years old!"
-
-#include<iostream>
-#include <string>
-using namespace std;
-
-
-int main() {
-    string name;
-    int age;
-    cout << "Enter your name : ";
-    getline(cin, name);
-    cout << "Enter your age : "; 
-    cin >> age;
-    cout << "Hello " << name << ", you are " << age << " years old." << endl;
-
-
-    return 0;
-}
-
 
 
 
